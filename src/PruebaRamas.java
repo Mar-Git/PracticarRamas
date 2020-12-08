2b15f3d2348a1cb17d6e85d28184a4f818a5e9b9
marit <75077118+Mar-Git@users.noreply.github.com>
2020-12-08 16:54:33 +0100

    He cambiado Hei por Saludos

--- src/PruebaRamas.java
+++ src/PruebaRamas.java
@@ -2,15 +2,12 @@
 public class PruebaRamas {
 
 	public static void main(String[] args) {
-		
-		public static void imprimeSaludo() { 
+	
 
-			System.out.println("Hei")	
+			System.out.println("Saludos");
 
 				
 
 		}
 
 	}
-
-}
