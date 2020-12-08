4a288d897aa6632264fc8fa25155443b6dc88b31
Mar-Git <75077118+Mar-Git@users.noreply.github.com>
2020-12-08 16:52:51 +0100

    Update PruebaRamas.java

--- src/PruebaRamas.java
+++ src/PruebaRamas.java
@@ -3,9 +3,9 @@ public class PruebaRamas {
 
 	public static void main(String[] args) {
 		
-		public static void imprimeSaludo() { 
+		
 
-			System.out.println("Hei")	
+			System.out.println("Wola")	
 
 				
 
